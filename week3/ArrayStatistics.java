0bd2377311d83cb52ac5fba4e8e7642978a368a1
Ishnika <ishnika25csu091@ncuindia.edu>
2026-08-23 20:44:41 +0530

    Add files via upload

--- week3/ArrayStatistics.java
+++ week3/ArrayStatistics.java
@@ -1,66 +1,70 @@
-public class ArrayStatistics {
-   public static int sum(int[] arr)
-   {
-        int sum=0;
-        for(int i=0;i<arr.length;i++)
-        {
-            sum+=arr[i];
+
+
+public class ArrayStatistics{
+
+    static int sum(int[] values) {
+        int total = 0;
+        for (int value : values) {
+            total += value;
         }
-       return sum;
-   }
-   public static double average(int[] arr)
-   {
-        int sum=sum(arr);
-        int count=arr.length;
-        double avg=(double)sum/count;
-        return avg;
-   }
-   public static int minimum(int []arr)
-   {
-        int min=arr[0];
-        for(int i=1;i<arr.length;i++)
-        {
-            if(min>arr[i])
-            {
-                min=arr[i];
+        return total;
+    }
+
+    static double average(int[] values) {
+        if (values.length == 0) {
+            return 0;
+        }
+        return (double) sum(values) / values.length;
+    }
+
+    static int minimum(int[] values) {
+        int min = values[0];
+        for (int value : values) {
+            if (value < min) {
+                min = value;
             }
         }
         return min;
-   }
-   public static int maximum(int []arr)
-   {
-        int max=arr[0];
-        for(int i=1;i<arr.length;i++)
-        {
-            if(max<arr[i])
-            {
-                max=arr[i];
+    }
+
+    static int maximum(int[] values) {
+        int max = values[0];
+
+        for (int value : values) {
+            if (value > max) {
+                max = value;
             }
         }
+
         return max;
-   }
-   public static int countEven(int []arr)
-   {
-        int count =0;
-        for(int i=0;i<arr.length;i++)
-        {
-            if(arr[i]%2==0)
-            {
-                count+=1;
+    }
+
+    static int countEven(int[] values) {
+        int count = 0;
+        for (int value : values) {
+            if (value % 2 == 0) {
+                count++;
             }
         }
-        return count ;
-   }
-   public static void signCount(int []arr)
-   {
-        int count=0;
-        for(int i=0;i<arr.length;i++)
-        {
-            if(arr[i]<0)
-            {
-                count+=1;
+        return count;
+    }
+
+    static void printSignCounts(int[] values) {
+        int positive = 0;
+        int negative = 0;
+        int zero = 0;
+        for (int value : values) {
+            if (value > 0) {
+                positive++;
+            } else if (value < 0) {
+                negative++;
+            } else {
+                zero++;
             }
         }
-        System.out.println("number of -ve sign:"+count);
-   }
+        System.out.println("Positive: " + positive);
+        System.out.println("Negative: " + negative);
+        System.out.println("Zero: " + zero);
+    }
+
 }
